a7640cd8b3b6d507e19bcd707dec539dc03c9e0c
Joseph Ruiz <joseph.ruiz.a@gmail.com>
2017-03-22 21:05:37 +0100

    Añadimos ejemplo asincrono_serie.js

---
+++ asincrono_serie.js
@@ -0,0 +1,30 @@
+/**
+ * Created by gambit on 22/03/17.
+ */
+"use strict";
+
+// Hacer una función que reciba un texto y tras 2 segundos lo escriba en la consola.
+
+function escribeTras2Segundos(texto, callback) {
+    setTimeout(function() {
+        console.log(texto);
+        callback();
+    }, 2000)
+}
+
+//llamar a una función n veces en serie
+// y al terminar llamar a un callback final
+function serie(veces, funcionALlamar, callbackFin){
+    if(veces==0){
+        callbackFin();
+        return;
+    }
+    veces--;
+    funcionALlamar('texto'+veces, function () {
+        serie(veces,funcionALlamar, callbackFin);
+    });
+}
+
+serie(5, escribeTras2Segundos, function () {
+   console.log("fin");
+});
