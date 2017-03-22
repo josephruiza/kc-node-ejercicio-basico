0f69eeb95b3e58f5df76355af776cdb4f8efb3e7
Joseph Ruiz <joseph.ruiz.a@gmail.com>
2017-03-22 20:41:47 +0100

    Añadimos ejemolo asincrono_paralelo.js

---
+++ asincrono_paralelo.js
@@ -0,0 +1,19 @@
+/**
+ * Created by gambit on 22/03/17.
+ */
+"use strict";
+
+// Hacer una función que reciba un texto y tras 2 segundos lo escriba en la consola.
+
+function escribeTras2Segundos(texto, callback) {
+    setTimeout(function() {
+        console.log(texto);
+        callback();
+    }, 2000)
+}
+
+for (var n = 0; n < 4; n++) {
+    escribeTras2Segundos('texto' + n, function() {
+        console.log('fin');
+    });
+}
