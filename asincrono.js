153248c9c8dec29345a97b5502a7a1e4c64eac91
Joseph Ruiz <joseph.ruiz.a@gmail.com>
2017-03-22 20:31:33 +0100

    Añadimos ejemolo asincrono.js

---
+++ asincrono.js
@@ -0,0 +1,19 @@
+/**
+ * Created by gambit on 22/03/17.
+ */
+'use strict';
+
+//Hacer una función que reciba un texto y tras 2 segundos lo escriba en la consola
+function escribeTras2Segundos(texto, callback){
+    setTimeout(function () {
+        console.log(texto);
+        callback();
+    } ,2000)
+}
+
+escribeTras2Segundos("texto1", function () {
+    escribeTras2Segundos("texto2", function () {
+        console.log("fin");
+    });
+});
+
